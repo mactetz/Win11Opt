fd0a9eb6bc7008ddcf045236027cc98af6308247
mactetz <77362348+mactetz@users.noreply.github.com>
2025-02-13 22:47:26 +0100

    add check for powershell version

--- Windows-Services-Stop.bat
+++ Windows-Services-Stop.bat
@@ -1,3 +1,18 @@
 @echo off
 echo run as admin
-powershell -ExecutionPolicy Unrestricted .\Windows-Services-Stop.ps1
+
+rem check pwoershell 5.x or greater
+SET POWERSHELL_PATH=%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe
+SET PWSH_PATH=%ProgramFiles%\PowerShell\7\pwsh.exe
+
+rem Überprüfen, ob PowerShell installiert ist
+IF EXIST "%POWERSHELL_PATH%" (
+    SET PSH_EXE=%POWERSHELL_PATH%
+)
+
+rem Überprüfen, ob PowerShell Core (pwsh) installiert ist
+IF EXIST "%PWSH_PATH%" (
+    SET PSH_EXE=%PWSH_PATH%
+)
+
+%PSH_EXE% -ExecutionPolicy Unrestricted .\Windows-Services-Stop.ps1
